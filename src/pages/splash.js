d3e70598ab86504a1df57726973a76fb59ee87ba
Claude Baxter <100107801+elborracho420@users.noreply.github.com>
2023-04-23 22:09:48 -0500

    Refactoring splash screen to use React state instead of directly manipulating the DOM

--- src/pages/splash.js
+++ src/pages/splash.js
@@ -1,48 +1,35 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function SplashScreen() {
+  const [displaySplashScreen, setDisplaySplashScreen] = useState(true);
+
   useEffect(() => {
     setTimeout(() => {
-      document.querySelector('.splash-screen').style.display = 'none';
+      setDisplaySplashScreen(false);
     }, 6000);
   }, []);
 
-  const pulseAnimation = {
-    '@keyframes pulse': {
-      '0%': {
-        transform: 'scale(0.8)',
-        opacity: 0.5,
-      },
-      '100%': {
-        transform: 'scale(1)',
-        opacity: 1,
-      },
-    },
-    animationName: 'pulse',
-    animationDuration: '4s',
-    animationTimingFunction: 'ease-in-out',
-    animationIterationCount: 1,
-  };
-
   return (
-    <div className="splash-screen" style={{ 
+    <div style={{ 
       backgroundColor: "#1c1c1c",
       height: "100vh",
       display: "flex",
       justifyContent: "center",
       alignItems: "center",
+      display: displaySplashScreen ? "flex" : "none"
     }}>
       <div style={{
         width: "200px",
         height: "200px",
-        border: "10px solid #fff",
-        borderRadius: "50%",
         backgroundColor: "#1c1c1c",
         backgroundImage: "url('images/Treats.svg')",
         backgroundSize: "103%",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
-        ...pulseAnimation,
+        animationName: "pulse",
+        animationDuration: "4s",
+        animationTimingFunction: "ease-in-out",
+        animationIterationCount: 1
       }}></div>
     </div>
   );
